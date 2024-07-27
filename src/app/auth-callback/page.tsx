936f25252084e03f8aff0b0fa7fc1b4067f30bce
Ahmad Badawi Kasyfur Rahman <ahmad.badawi.k@mail.ugm.ac.id>
2024-07-27 15:46:35 +0700

    setup prisma

--- src/app/auth-callback/page.tsx
+++ src/app/auth-callback/page.tsx
@@ -1,14 +1,52 @@
-import { useRouter, useSearchParams } from "next/navigation"
+"use client";
 
-const Page = async () => {
-    const router = useRouter()
+import { useRouter, useSearchParams } from 'next/navigation';
+import { trpc } from '../_trpc/client';
+import { Loader2 } from 'lucide-react';
+import { useEffect } from 'react';
 
-    const searchParams = useSearchParams()
-    const origin = searchParams.get('origin')
+const Page = () => {
+  const router = useRouter();
+  const searchParams = useSearchParams();
+  const origin = searchParams.get('origin');
 
-    const apiResponse = await fetch('/api/whatever')
-    const data = await apiResponse.json()
+  const { data, error, isLoading } = trpc.authCallback.useQuery(undefined, {
+    retry: true,
+    retryDelay: 500,
+  });
 
+  useEffect(() => {
+    if (data) {
+      // user is synced to db
+      router.push(origin ? `/${origin}` : '/dashboard');
+    } else if (error?.data?.code === 'UNAUTHORIZED') {
+      router.push('/sign-in');
+    }
+  }, [data, error, origin, router]);
+
+  if (isLoading) {
+    return (
+      <div className='w-full mt-24 flex justify-center'>
+        <div className='flex flex-col items-center gap-2'>
+          <Loader2 className='h-8 w-8 animate-spin text-slate-800' />
+          <h3 className='font-semibold text-xl'>
+            Setting up your account...
+          </h3>
+          <p>You will be redirected automatically.</p>
+        </div>
+      </div>
+    );
+  }
+
+  return (
+    <div className='w-full mt-24 flex justify-center'>
+      <div className='flex flex-col items-center gap-2'>
+        <h3 className='font-semibold text-xl'>
+          Processing...
+        </h3>
+      </div>
+    </div>
+  );
 }
 
-export default Page
+export default Page;
--- src/trpc/index.ts
+++ src/trpc/index.ts
@@ -1,9 +1,37 @@
-import { router } from './trpc';
- 
+import { getKindeServerSession } from '@kinde-oss/kinde-auth-nextjs/server';
+import { publicProcedure, router } from './trpc';
+import { TRPCError } from '@trpc/server';
+import { db } from '@/db';
+
 export const appRouter = router({
-  // ...
+  authCallback: publicProcedure.query(async () => {
+    const { getUser } = getKindeServerSession();
+    const user = await getUser();
+
+    if (!user?.id || !user?.email) {
+      throw new TRPCError({ code: 'UNAUTHORIZED' });
+    }
+
+    // check if the user is exists in the db
+    const dbUser = await db.user.findFirst({
+        where: {
+            id: user.id
+        }
+    })
+
+    if(!dbUser) {
+        // Create user in db
+        await db.user.create({
+            data: {
+                id: user.id,
+                email: user.email
+                
+            }
+        })
+    }
+  }),
 });
- 
+
 // Export type router type signature,
 // NOT the router itself.
 export type AppRouter = typeof appRouter;
