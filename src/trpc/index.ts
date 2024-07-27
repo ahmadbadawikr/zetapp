f58c9fc8dc865db0640c0fa9b32ddd416c062827
Ahmad Badawi Kasyfur Rahman <ahmad.badawi.k@mail.ugm.ac.id>
2024-07-28 00:18:19 +0700

    making dashboard items

--- src/trpc/index.ts
+++ src/trpc/index.ts
@@ -1,7 +1,8 @@
 import { getKindeServerSession } from '@kinde-oss/kinde-auth-nextjs/server';
-import { publicProcedure, router } from './trpc';
+import { privateProcedure, publicProcedure, router } from './trpc';
 import { TRPCError } from '@trpc/server';
 import { db } from '@/db';
+import {z} from "zod"
 
 export const appRouter = router({
   authCallback: publicProcedure.query(async () => {
@@ -25,11 +26,40 @@ export const appRouter = router({
             data: {
                 id: user.id,
                 email: user.email
-                
+
             }
         })
     }
   }),
+  getUserFiles: privateProcedure.query(async ({ctx})=>{
+    const {userId, user} = ctx
+    
+    return await db.file.findMany({
+      where: {
+        userId
+      }
+    })
+  }),
+  deleteFile: privateProcedure
+    .input(z.object({id: z.string()}))
+    .mutation(async ({ctx,input})=>{
+        const {userId}= ctx
+        const file= await db.file.findFirst({
+          where: {
+            id: input.id,
+            userId,
+          }
+        })
+
+        if(!file) throw new TRPCError({code: "NOT_FOUND"})
+        await db.file.delete({
+          where: {
+            id: input.id
+          }
+        })
+
+        return file
+    })
 });
 
 // Export type router type signature,
