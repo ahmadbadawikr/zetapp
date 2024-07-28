c38429b82e04511b19cd1233d305a1ec694cb115
Ahmad Badawi Kasyfur Rahman <ahmad.badawi.k@mail.ugm.ac.id>
2024-07-28 20:18:26 +0700

    pdf renderer

--- src/components/PdfRenderer.tsx
+++ src/components/PdfRenderer.tsx
@@ -1,13 +1,39 @@
-const PdfRenderer = () => {
-    return (
-        <div className="w-full bg-white rounded-md shadow flex flex-col items-center">
-            <div className="h-14 w-full border-b border-slate-200 flex item-center justify-between" >
-                <div className="flex mx-5 items-center gap-1.5">
-                    top bar
-                </div>
-            </div>
+"use client";
+
+import { Document, Page, pdfjs } from "react-pdf";
+import 'react-pdf/dist/Page/AnnotationLayer.css';
+import 'react-pdf/dist/Page/TextLayer.css';
+
+pdfjs.GlobalWorkerOptions.workerSrc = new URL(
+    'pdfjs-dist/build/pdf.worker.min.mjs',
+    import.meta.url,
+  ).toString();
+  
+interface PdfRendererProps {
+  url: string;
+}
+
+const PdfRenderer: React.FC<PdfRendererProps> = ({ url }) => {
+  return (
+    <div className="w-full bg-white rounded-md shadow flex flex-col items-center">
+      <div className="h-14 w-full border-b border-slate-200 flex items-center justify-between">
+        <div className="flex mx-5 items-center gap-1.5">
+          top bar
         </div>
-    )
+      </div>
+      <div className="flex-1 w-full max-h-screen overflow-auto">
+        <div>
+          <Document
+            file={url}
+            className="max-h-full"
+            onLoadError={(error) => console.error('Error while loading document:', error)}
+          >
+            <Page pageNumber={1} />
+          </Document>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default PdfRenderer
--- src/components/UploadButton.tsx
+++ src/components/UploadButton.tsx
@@ -3,7 +3,7 @@
 import { useState } from "react";
 import { Dialog, DialogTrigger, DialogContent, DialogTitle } from "./ui/dialog";
 import { Button } from "./ui/button";
-import { Cloud, File } from "lucide-react";
+import { Cloud, File, Loader2 } from "lucide-react";
 import Dropzone from "react-dropzone";
 import { Progress } from "./ui/progress";
 import { useUploadThing } from "@/lib/uploadthing";
@@ -14,7 +14,7 @@ import { useRouter } from "next/navigation";
 const UploadDropzone = () => {
     const router = useRouter()
 
-    const [isUploading, setIsUploading] = useState<boolean>(true)
+    const [isUploading, setIsUploading] = useState<boolean>(false)
     const [uploadProgress, setUploadProgress] = useState<number>(0)
     const {startUpload} = useUploadThing("imageUploader")
     const { toast } = useToast()
@@ -110,8 +110,16 @@ const UploadDropzone = () => {
                                 {isUploading ? (
                                     <div className="w-full mt-4 max-w-xs mx-auto">
                                         <Progress 
+                                            indicatorColor={ uploadProgress === 100 ? 'bg-green-500' : ''}
                                             value={uploadProgress} 
-                                            className="h-1 w-full bg-slate-200"/>
+                                            className="h-1 w-full bg-slate-200"
+                                        />
+                                        {uploadProgress === 100 ? (
+                                            <div className="flex gap-1 items-center justify-center text-sm text-slate-700 text-center p-2">
+                                                <Loader2 className="h-3 w-3 animate-spin"/>
+                                                Redirecting
+                                            </div>
+                                        ) : null }
                                     </div>
                                 ) : null}
                                 <input
