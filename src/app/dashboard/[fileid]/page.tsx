cec9e99610749b961bb31777ae89b871170d7df1
Ahmad Badawi Kasyfur Rahman <ahmad.badawi.k@mail.ugm.ac.id>
2024-07-29 09:19:19 +0700

    fix pdf renderer

--- src/app/dashboard/[fileid]/page.tsx
+++ src/app/dashboard/[fileid]/page.tsx
@@ -1,51 +1,58 @@
-import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server"
-import { notFound, redirect } from "next/navigation"
-import { db } from "@/db"
-import PdfRenderer from "@/components/PdfRenderer"
-import ChatWrapper from "@/components/ChatWrapper"
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
+import { notFound, redirect } from "next/navigation";
+import { db } from "@/db";
+import PdfRenderer from "@/components/PdfRenderer";
+import ChatWrapper from "@/components/ChatWrapper";
 
 interface PageProps {
-    params: {
-        fileid: string
-    }
+  params: {
+    fileid: string;
+  };
 }
-const Page = async ({params}: PageProps) => {
-    // retrieve the file id
-    const {fileid} = params
-    // get user id
-    const {getUser} = getKindeServerSession()
-    const user = await getUser()
 
-    if(!user || !user.id) redirect (`/auth-callback?origin=dashboard/${fileid}`) 
+const Page = async ({ params }: PageProps) => {
+  // retrieve the file id
+  const { fileid } = params;
 
+  // get user id
+  const { getUser } = getKindeServerSession();
+  const user = await getUser();
 
-    // make database call
-    const file = await db.file.findFirst({
-        where: {
-            id: fileid,
-            userId: user.id
-        }
-    })
+  if (!user || !user.id) {
+    redirect(`/auth-callback?origin=dashboard/${fileid}`);
+    return;
+  }
 
-    if (!file) notFound()
+  // make database call
+  const file = await db.file.findFirst({
+    where: {
+      id: fileid,
+      userId: user.id,
+    },
+  });
 
-    return (
-        <div className="flex-1 justify-between flex flex-col h-[calc(100vh-3.5rem)]">
-            <div className="mx-auto w-full max-w-8xl grow lg:flex xl:px-2">
-                {/** left side **/}
-                <div className="flex-1 xl:flex">
-                    <div className="px-4 py-6 sm:px-6 lg:pl-8 xl:flex-1 xl:pl-6">
-                        <PdfRenderer url={file.url}/>
-                    </div>
-                </div>
+  if (!file) {
+    notFound();
+    return;
+  }
 
-                {/** right side **/}
-                <div className="shrink-0 flex-[0.75] border-t border-gray-200 lg:w-96 lg:border-l lg:border-t-0">
-                    <ChatWrapper/>
-                </div>
-            </div>
+  return (
+    <div className="flex-1 justify-between flex flex-col h-[calc(100vh-3.5rem)]">
+      <div className="mx-auto w-full max-w-8xl grow lg:flex xl:px-2">
+        {/** left side **/}
+        <div className="flex-1 xl:flex">
+          <div className="px-4 py-6 sm:px-6 lg:pl-8 xl:flex-1 xl:pl-6">
+            <PdfRenderer url={file.url} />
+          </div>
         </div>
-    )
-}
 
-export default Page
+        {/** right side **/}
+        <div className="shrink-0 flex-[0.75] border-t border-gray-200 lg:w-96 lg:border-l lg:border-t-0">
+          <ChatWrapper />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Page;
--- src/components/PdfRenderer.tsx
+++ src/components/PdfRenderer.tsx
@@ -1,9 +1,38 @@
 "use client";
 
-import { Document, Page, pdfjs } from "react-pdf";
-import 'react-pdf/dist/Page/AnnotationLayer.css';
-import 'react-pdf/dist/Page/TextLayer.css';
-
+import {
+    ChevronDown,
+    ChevronUp,
+    Loader2,
+    RotateCw,
+    Search,
+  } from 'lucide-react'
+  import { Document, Page, pdfjs } from 'react-pdf'
+  
+  import 'react-pdf/dist/Page/AnnotationLayer.css'
+  import 'react-pdf/dist/Page/TextLayer.css'
+  import { useToast } from './ui/use-toast'
+  
+  import { useResizeDetector } from 'react-resize-detector'
+  import { Button } from './ui/button'
+//   import { Input } from './ui/input'
+  import { useState } from 'react'
+  
+  import { useForm } from 'react-hook-form'
+  import { z } from 'zod'
+  
+//   import { zodResolver } from '@hookform/resolvers/zod'
+  import { cn } from '@/lib/utils'
+//   import {
+//     DropdownMenu,
+//     DropdownMenuContent,
+//     DropdownMenuItem,
+//     DropdownMenuTrigger,
+//   } from './ui/dropdown-menu'
+  
+//   import SimpleBar from 'simplebar-react'
+//   import PdfFullscreen from './PdfFullscreen'
+  
 pdfjs.GlobalWorkerOptions.workerSrc = new URL(
     'pdfjs-dist/build/pdf.worker.min.mjs',
     import.meta.url,
@@ -14,6 +43,52 @@ interface PdfRendererProps {
 }
 
 const PdfRenderer: React.FC<PdfRendererProps> = ({ url }) => {
+    const { toast } = useToast()
+    const [numPages, setNumPages] = useState<number>()
+    const [currPage, setCurrPage] = useState<number>(1)
+    const [scale, setScale] = useState<number>(1)
+    const [rotation, setRotation] = useState<number>(0)
+    const [renderedScale, setRenderedScale] = useState<
+      number | null
+    >(null)
+  
+    const isLoading = renderedScale !== scale
+
+    const CustomPageValidator = z.object({
+      page: z
+        .string()
+        .refine(
+          (num) => Number(num) > 0 && Number(num) <= numPages!
+        ),
+    })
+  
+    type TCustomPageValidator = z.infer<
+      typeof CustomPageValidator
+    >
+  
+    const {
+      register,
+      handleSubmit,
+      formState: { errors },
+      setValue,
+    } = useForm<TCustomPageValidator>({
+      defaultValues: {
+        page: '1',
+      },
+    //   resolver: zodResolver(CustomPageValidator),
+    })
+  
+    console.log(errors)
+  
+    const { width, ref } = useResizeDetector()
+  
+    const handlePageSubmit = ({
+      page,
+    }: TCustomPageValidator) => {
+      setCurrPage(Number(page))
+      setValue('page', String(page))
+    }
+  
   return (
     <div className="w-full bg-white rounded-md shadow flex flex-col items-center">
       <div className="h-14 w-full border-b border-slate-200 flex items-center justify-between">
@@ -22,18 +97,54 @@ const PdfRenderer: React.FC<PdfRendererProps> = ({ url }) => {
         </div>
       </div>
       <div className="flex-1 w-full max-h-screen overflow-auto">
-        <div>
-          <Document
-            file={url}
-            className="max-h-full"
-            onLoadError={(error) => console.error('Error while loading document:', error)}
-          >
-            <Page pageNumber={1} />
-          </Document>
-        </div>
+      <Document
+              loading={
+                <div className='flex justify-center'>
+                  <Loader2 className='my-24 h-6 w-6 animate-spin' />
+                </div>
+              }
+              onLoadError={() => {
+                toast({
+                  title: 'Error loading PDF',
+                  description: 'Please try again later',
+                  variant: 'destructive',
+                })
+              }}
+              onLoadSuccess={({ numPages }) =>
+                setNumPages(numPages)
+              }
+              file={url}
+              className='max-h-full'>
+              {isLoading && renderedScale ? (
+                <Page
+                  width={width ? width : 1}
+                  pageNumber={currPage}
+                  scale={scale}
+                  rotate={rotation}
+                  key={'@' + renderedScale}
+                />
+              ) : null}
+
+              <Page
+                className={cn(isLoading ? 'hidden' : '')}
+                width={width ? width : 1}
+                pageNumber={currPage}
+                scale={scale}
+                rotate={rotation}
+                key={'@' + scale}
+                loading={
+                  <div className='flex justify-center'>
+                    <Loader2 className='my-24 h-6 w-6 animate-spin' />
+                  </div>
+                }
+                onRenderSuccess={() =>
+                  setRenderedScale(scale)
+                }
+              />
+            </Document>
       </div>
     </div>
   );
-}
+};
 
-export default PdfRenderer
+export default PdfRenderer;
